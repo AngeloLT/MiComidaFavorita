87c3b3f1ed8a0e3dfb4acd68f92d2ae07730eebb
Angelo Lopez Torrico <angelo.lt.91@gmail.com>
2025-01-22 23:24:22 -0400

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+  // Pegar configuración de Firebase Console
+  apiKey: "AIzaSyAUyr4xcBbQRoCUjnm6y37oEQ3b8Bf_55o",
+  authDomain: "micomidafavorita-e93d2.firebaseapp.com",
+  projectId: "micomidafavorita-e93d2",
+  storageBucket: "micomidafavorita-e93d2.firebasestorage.app",
+  messagingSenderId: "360157369581",
+  appId: "t1:360157369581:web:e40eb4a9a9aeddf59de8e4"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
